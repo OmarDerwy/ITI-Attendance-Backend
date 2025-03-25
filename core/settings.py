ad1eba74224eefb0a087fae3a77265b4e3414d74
OmarDerwy <OmarDerwy@gmail.com>
2025-03-25 15:49:10 +0200

    fix(users): fix users not able to authenticate
    simple jwt wasn't properly taking the bearer token correctly
    +added script in bruno that automatically saves jwt in environment variable to use on other endpoints

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'users',
     'djoser',
     'drf_spectacular',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -134,7 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
@@ -161,10 +162,16 @@ DJOSER = {
         'user_delete': 'djoser.serializers.UserDeleteSerializer',
     },
     'USER_ID_FIELD': 'id',
+    # 'PERMISSIONS': {
+    #     'user': ['rest_framework.permissions.AllowAny'],
+    #     'user_list': ['rest_framework.permissions.AllowAny'],
+    # },
 }
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+   'AUTH_HEADER_TYPES': ('Bearer',),
+   'ACCESS_TOKEN_LIFETIME': timedelta(weeks=1),
+   'REFRESH_TOKEN_LIFETIME': timedelta(weeks=1),
 }
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Attendance Project API',
