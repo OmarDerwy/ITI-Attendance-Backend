4ee3489d70d39fa47c7fea9222743430ad9bb2a0
Hoda-ArtCoder <hmag425@gmail.com>
2025-03-27 22:15:48 +0200

    Revert "Ap 24 allow supervisors to define class schedules "

--- core/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-
-@api_view(['GET'])
-def hello_world(request):
-    return Response("Hello, World!", status=status.HTTP_200_OK)
--- lost_and_found_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LostAndFoundSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lost_and_found_system'
