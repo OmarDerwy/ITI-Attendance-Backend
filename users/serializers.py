879134261bb4b2575f8e2dd4feaa372f715b3498
OmarDerwy <OmarDerwy@gmail.com>
2025-03-28 20:53:58 +0200

    finish

--- users/serializers.py
+++ users/serializers.py
@@ -9,17 +9,13 @@ User = get_user_model()
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'email', 'password']
         
-    def perform_create(self, validated_data):
-        # Make sure username is set from email
-        validated_data['username'] = validated_data.get('email')
-        return super().perform_create(validated_data)
 
 class CustomUserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         model = User
-        fields = ['id', 'email', 'groups','username', 'first_name', 'last_name', 'phone_number', 'phone_uuid', 'laptop_uuid']
+        fields = ['id', 'email', 'groups', 'first_name', 'last_name', 'phone_number', 'phone_uuid', 'laptop_uuid']
 class CustomSelfUserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         model = User
