9fe98ee888fd2231f0d80bb4be9b13ea94b7959c
Hoda Magdy <hmag425@gmail.com>
2025-04-02 00:47:22 +0200

    refactor-remove unused commented code

--- attendance_management/views/branch_views.py
+++ attendance_management/views/branch_views.py
@@ -5,21 +5,6 @@ from core import permissions
 from users.models import CustomUser
 
 class BranchViewSet(viewsets.ModelViewSet):
-    # def get_queryset(self):
-    #     user: CustomUser = self.request.user
-    #     user_groups = user.groups.all().values_list('name', flat=True)
-    #     if 'admin' in user_groups:
-    #         return Branch.objects.all()
-    #     if 'supervisor' in user_groups:
-    #         branches = Branch.objects.all()
-    #         branches = branches.tracks_set(supervisor=user)
-    #     if 'student' in user_groups:
-    #         branches = Branch.objects.all()
-    #         branches = branches.tracks_set() # TODO continue this logic
-    #         return 
-    #     return super().get_queryset()
-    
-
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
     permission_classes = [permissions.IsAdminUser]
