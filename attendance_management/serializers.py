bcaa9fa5256aa216204106a76baa5ce566eca8eb
Hoda Magdy <hmag425@gmail.com>
2025-04-02 05:35:16 +0200

    feat add program-type to track and include Ids in track serrializer

--- attendance_management/serializers.py
+++ attendance_management/serializers.py
@@ -19,11 +19,19 @@ class StudentSerializer(serializers.ModelSerializer):  # Updated to use Student
         fields = ['id', 'user', 'track']
 
 class TrackSerializer(serializers.ModelSerializer):
+    branch_id = serializers.IntegerField(source='default_branch.id', read_only=True)
+    track_id = serializers.IntegerField(source='id', read_only=True)
     default_branch = serializers.StringRelatedField()
+    supervisor = serializers.SerializerMethodField()  # Get the supervisor's first and last name
 
+    def get_supervisor(self, obj):
+        if obj.supervisor:
+            return f"{obj.supervisor.first_name} {obj.supervisor.last_name} "
+        return None
+    program_type = serializers.CharField(source='get_program_type_display', read_only=True)
     class Meta:
         model = Track
-        fields = ['id', 'name', 'supervisor', 'intake', 'start_date', 'description', 'default_branch']
+        fields = '__all__'  # Ensure branch_id and track_id are included
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,5 +46,6 @@ class AttendanceRecordSerializer(serializers.ModelSerializer):
 class PermissionRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = PermissionRequest
+        
         fields = ['id', 'student', 'schedule', 'request_type', 'reason', 'status', 'created_at', 'updated_at']
         read_only_fields = ['id', 'created_at', 'updated_at', 'status']
