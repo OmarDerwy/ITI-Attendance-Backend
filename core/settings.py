4e3c821a267d88bb4c9ec1bb0da2579c7593234b
OmarDerwy <OmarDerwy@gmail.com>
2025-05-06 16:18:58 +0300

    hotfix

--- core/settings.py
+++ core/settings.py
@@ -221,8 +221,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': os.environ.get('PAGE_SIZE', 10),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+if DEBUG:
+    REST_FRAMEWORK['DEFAULT_SCHEMA_CLASS'] = 'drf_spectacular.openapi.AutoSchema'
 
 AUTHENTICATION_BACKENDS = [
     "djoser.auth_backends.LoginFieldBackend",
